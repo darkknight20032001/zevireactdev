982389b57884aae2187f47f422e5bb7cebe2bc38
darkknight20032001 <84170745+darkknight20032001@users.noreply.github.com>
2023-12-12 11:17:28 +0530

    synced

--- src/Components/HomePage/HomePage.tsx
+++ src/Components/HomePage/HomePage.tsx
@@ -1,28 +1,14 @@
 import React from "react";
 import "./HomePage.sass";
 import SearchBar from "../Functionalities/SearchBar/SearchBar";
-import { useNavigate } from "react-router-dom";
-import { Button, Stack } from "@mui/material";
+
 
 const HomePage = () => {
   return (
     <div className="MainPageBox">
       <div className="HomePageBox">
-     
         <SearchBar />
-        
-
-        {/* <button
-        onClick={(e) => {
-          e.preventDefault();
-          navigate("/productSection");
-        }}
-      >
-        Continue here...
-      </button> */}
-    
       </div>
-      
     </div>
   );
 };
